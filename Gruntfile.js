d17f5abfdd3a9127d5af1da45df1173bf7bc1941
Gaurav Ramesh <gnramesh@paypal.com>
2015-09-06 12:04:34 -0700

    add Gruntfile and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+var path = require('path');
+
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            all: {
+                src: [ 'index.js', 'src/**/*.js', 'test/**/*.js' ]
+            }
+        },
+
+        mochacli: {
+            options: {
+                require: [ 'chai' ]
+            },
+            all: [ 'test/**/*.js' ]
+        },
+
+        dustjs: {
+            compile: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/templates/',
+                    src: '**/*.dust',
+                    dest: 'dist/templates',
+                    ext: '.js'
+                }],
+                options: {
+                    fullname: function(filepath) {
+                        var dirName = path.dirname(filepath).split(path.sep).slice(2).join(path.sep),
+                            templateName =  (dirName ? (dirName + path.sep) : '') + path.basename(filepath, '.dust');
+                        console.log("dirname" + templateName);
+                        return templateName;
+                    }
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-dustjs');
+    grunt.loadNpmTasks('grunt-mocha-cli');
+
+    grunt.registerTask('test', [ 'mochacli' ]);
+    grunt.registerTask('default', []);
+    grunt.registerTask('build', [ 'jshint', 'dustjs', 'mochacli' ]);
+
+};
